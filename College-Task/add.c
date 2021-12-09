179b320c399eab5786672cc362234640b1ff1c1a
rock12231 <37548802+rock12231@users.noreply.github.com>
2021-12-09 18:03:36 +0530

    ädd.c exe prt.c

---
+++ College-Task/add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+
+    int a,b,sum=0;
+    printf("Enter first number ");
+    scanf("%d",&a);
+    printf("Enter second number ");
+    scanf("%d", &b);
+    sum = a + b ;
+    printf("Your Sum : %d",sum);
+    return 0;
+}
